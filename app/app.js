348f9f1e0183f35f37e8310c0c94b9b5f9a9b615
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2019-01-31 20:25:28 +0200

    finished mobile design

--- app/app.js
+++ app/app.js
@@ -10,3 +10,21 @@ const ellipsisMenu = document.querySelector('.ellipsis-menu');
 ellipsis.addEventListener('click', function () {
   ellipsisMenu.classList.toggle('showing');
 })
+
+
+// carousel la-de-da
+// every 3 seconds, toggle
+setInterval(toggle, 5000);
+
+// if you click the arrows, toggle.
+const arrow = document.querySelector('.arrows');
+arrow.addEventListener('click', toggle());
+
+function toggle() {
+
+  let banners = document.querySelectorAll('.banner');
+  for (banner of banners) {
+    banner.classList.toggle('hide');
+    setTimeout(banner.classList.toggle('shrink'), 700);
+  }
+}
